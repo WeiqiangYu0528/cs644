0db44e067734fded710532d2e86dde3565312431
Daniel Ephrat <dephrat@uwaterloo.ca>
2024-02-16 18:42:10 -0500

    Hierarchy rule 4: Class can't extend final class

--- src/ast/hierarchyVisitor.cpp
+++ src/ast/hierarchyVisitor.cpp
@@ -1,4 +1,5 @@
 #include <unordered_set>
+#include <cassert>
 #include "hierarchyVisitor.hpp"
 
 HierarchyVisitor::HierarchyVisitor(std::shared_ptr<SymbolTable> st, std::unordered_map<std::string, 
@@ -37,6 +38,25 @@ void HierarchyVisitor::visit(std::shared_ptr<ClassDecl> n) {
             }
         }
     }
+
+    //Rule #4: Class must not extend final class
+    for (std::shared_ptr<IdentifierType> ext : n->extended) {
+        for (const auto& entry : tables) {
+            if (entry.second.contains(ext->id->name)) {
+                std::shared_ptr<ClassDecl> classDecl = std::dynamic_pointer_cast<ClassDecl>(entry.second.at(ext->id->name)->getAst()->classOrInterfaceDecl);
+                assert(classDecl != nullptr); //Rule #1 already ensures that you can't extend an interface
+                if (classDecl->modifier == "final") {
+                    std::cerr << "Error: Class " << n->id->name << " extends final Class " << classDecl->id->name << std::endl;
+                    error = true;
+                    return;
+                }
+            }
+        }
+    }
+
+
+
+
     //Rule #2: Class must not implement a class
     //Rule #3: An interface must not be repeated in a class's implements clause
     std::unordered_set<std::string> implementedInterfaces;
