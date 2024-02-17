4abe51e2b800c0fa21e6adaac29cc3c0b511858d
Weiqiang Yu <w8yu@uwaterloo.ca>
2024-02-16 19:53:25 -0500

    solve more error cases

--- a2_test.py
+++ a2_test.py
@@ -65,6 +65,8 @@ def main(start_dir):
                 error_type = "TYPE_LINKING"
             if hierarchy:
                 error_type = "HIERARCHY"
+            else:
+                error_type = "OTHER"
             incorrect_items.append(f"{item_string}\033[80G{error_type}")
         else:
             correct += 1
