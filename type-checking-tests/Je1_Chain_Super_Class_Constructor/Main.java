2ecd60ce89d7efce7e36a13990330d6766b8b51a
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-03-06 23:49:28 -0500

    Add test cases

---
+++ type-checking-tests/Je1_Chain_Super_Class_Constructor/Main.java
@@ -0,0 +1,5 @@
+public class Main extends A {
+
+    public Main(String s){}
+
+}
---
+++ type-checking-tests/Je3_Wrong_Constructor_Invocation_Type/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public Main(String str, int s) {
+    }
+    public static int test() {
+        Main m = new Main("hello");
+        return 123;
+    }
+}
