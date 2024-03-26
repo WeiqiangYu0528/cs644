87730699ac3c1000ac876e14528f98dd4b9be1b9
Daniel Ephrat <dephrat@uwaterloo.ca>
2024-03-26 15:56:35 -0400

    Copy fixes

--- include/Copy.hpp
+++ include/Copy.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+#include <type_traits>
 //package joosc.util;
 
 /**
@@ -8,15 +10,14 @@
  */
 
 //abstract virtual class
-#include <type_traits>
-template <class T>
+template <typename T>
 class Copy : public Cloneable {
 public:
-    T copy();
+    virtual std::shared_ptr<T> copy() = 0;
     class Util {
     public:        
         template<typename A> 
-        typename std::enable_if<std::is_base_of<A, T>::value && std::is_base_of<Copy<A>, T>::value, T>::type
-        copy(T o);
+        typename std::enable_if<std::is_base_of<A, T>::value && std::is_base_of<Copy<A>, T>::value, std::shared_ptr<T>>::type
+        copy(std::shared_ptr<T> o);
     };
 };
--- src/tir-c++/Copy.cpp
+++ src/tir-c++/Copy.cpp
@@ -1,8 +1,8 @@
 #include "Copy.hpp"
 
-template<class T>
-template<typename A>
-typename std::enable_if<std::is_base_of<A, T>::value && std::is_base_of<Copy<A>, T>::value, T>::type
-Copy<T>::Util::copy(T o) {
-    return static_cast<T>(o.copy());
+template<typename T>
+template<typename A> 
+typename std::enable_if<std::is_base_of<A, T>::value && std::is_base_of<Copy<A>, T>::value, std::shared_ptr<T>>::type
+Copy<T>::Util::copy(std::shared_ptr<T> o) {
+    return std::dynamic_pointer_cast<T>(o->copy());
 }
