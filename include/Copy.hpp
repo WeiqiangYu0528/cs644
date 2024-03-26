8cb941c0dc169d691aaef4879e603aa36e6513e8
Daniel Ephrat <dephrat@uwaterloo.ca>
2024-03-26 14:26:20 -0400

    Copy.hpp, Copy.cpp

---
+++ include/Copy.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+//package joosc.util;
+
+/**
+ * Interface used to copy objects.  Similar to {@code Cloneable}, except that
+ * {@code copy()} must be public, not protected as {@code clone()} is.
+ */
+
+//abstract virtual class
+#include <type_traits>
+template <class T>
+class Copy : public Cloneable {
+public:
+    T copy();
+    class Util {
+    public:        
+        template<typename A> 
+        typename std::enable_if<std::is_base_of<A, T>::value && std::is_base_of<Copy<A>, T>::value, T>::type
+        copy(T o);
+    };
+};
