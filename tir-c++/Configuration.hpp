6981b83f9b43da05d9843cead42755d537fba97a
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2024-03-25 20:40:34 -0400

    Add configuration and main C++ impl

---
+++ tir-c++/Configuration.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class Configuration {
+public:
+    static const std::string ABSTRACT_ARG_PREFIX;
+    static const std::string ABSTRACT_RET;
+
+    static const int WORD_SIZE;
+};
