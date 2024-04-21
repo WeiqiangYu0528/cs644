7592980b94ce49986c34301fab910be116646cad
Bing Chen <b335chen@uwaterloo.com>
2024-04-20 21:59:29 -0400

    add new test cases

---
+++ tests/our-a4-tests/reachability/J1_lastreturn3.java
@@ -0,0 +1,13 @@
+public class J1_lastreturn3 {
+    public J1_lastreturn3() {}
+    public void test(int a) {
+        int c = 0;
+        if(0 < 0)
+            return;
+        if (a > 0) {
+            return;
+        } else if (a < 0) {
+            return;
+        }
+    }
+}
