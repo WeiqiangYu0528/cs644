bf7dbeb2d192cbf4e7fd0011166942a0bdab9265
Daniel Ephrat <dephrat@uwaterloo.ca>
2024-04-24 23:03:56 -0400

    tests

---
+++ J1_modfield.java
@@ -0,0 +1,11 @@
+public class J1_modfield{
+public J1_modfield(){}
+public static int x;
+public static int test(){
+	return new J1_modfield().temp(3);
+}
+public int temp(int y){
+x = y;
+return 5;
+}
+}
