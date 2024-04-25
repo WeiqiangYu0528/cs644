4c5b8631342bbdd9fe4d9eb314d7615da2dbd94f
Daniel Ephrat <dephrat@uwaterloo.ca>
2024-04-24 23:45:58 -0400

    a6 tests

--- tests/our-a6-tests/J1_modfield.java
+++ tests/our-a6-tests/J1_modfield.java
@@ -1,11 +1,11 @@
 public class J1_modfield{
 public J1_modfield(){}
-public static int x;
+protected static int x;
 public static int test(){
 	return new J1_modfield().temp(3);
 }
 public int temp(int y){
-x = y;
-return 5;
+J1_modfield.x = y;
+return 123;
 }
 }
